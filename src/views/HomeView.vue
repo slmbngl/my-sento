76638fc54da42bfa96a47b1b7cf0c9d5e7cb0712
MehmetSalimBingol <salim.bingol@hotmail.com>
2024-07-14 21:12:20 +0300

    Responsive part: progressSlider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
       <section class="relative bg-white z-10">
         <Progress />
         <div class="flex mt-0 xl:mt-20 pl-20 pb-5">
-          <div class="w-3/4 mr-0 md:mr-20">
+          <div class="w-full md:w-3/4 mr-0 md:mr-20">
             <p class="text-7xl md:text-9xl font-extrabold text-left text-black font-qubi">Faydaları.</p>
             <p class="text-xl font-normal text-left text-black font-qubi">Çalışanın, zihnini meşgul eden kişisel
               sorunları
