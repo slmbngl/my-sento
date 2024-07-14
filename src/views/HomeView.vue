5c4805d1b8f4f6344463d0d8fb53c3b505ee7cd2
MehmetSalimBingol <salim.bingol@hotmail.com>
2024-07-14 18:32:46 +0300

    Vue Router installation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -54,7 +54,7 @@
                   d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
               </svg></button>
           </div>
-          <img class="w-1/5 h-full right-0 mr-0" src="./components/images/ravi-patel.jpg" alt="">
+          <img class="w-1/5 h-full right-0 mr-0" src="../components/images/ravi-patel.jpg" alt="">
         </div>
         <div class="relative h-screen bg-footer-texture ">
           <div class="flex h-full justify-center items-center">
